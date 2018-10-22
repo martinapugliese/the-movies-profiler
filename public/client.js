319f96abf21098e159008c7ba4b1a675f0ea9fd4
martinapugliese <m.letitbe@gmail.com>
2018-10-22 16:26:15 +0200

    passed object of numerical values to client

--- public/client.js
+++ public/client.js
@@ -1,6 +1,3 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
 $(function() {  
   
     $('form').submit(function(event) {
@@ -10,19 +7,17 @@ $(function() {
       
       console.log(data);
       
-      $("#p1").css("Font-Weight","Bold").text("Here's the overview of the movie:")
-      $("#movie-overview").text(data.movies.slice(-1)[0].overview)
-      $("#movie-title").text("Title " + data.movies.slice(-1)[0].title)
-      $("#movie-original-title").text("Original title " + data.movies.slice(-1)[0].originalTitle)
-      $("#movie-original-language").text("Original language " + data.movies.slice(-1)[0].originalLanguage)
-      $("#movie-score").text("Score " + data.movies.slice(-1)[0].score)
-      $("#movie-cast-score").text("Cast score: " + data.movies.slice(-1)[0].castScore)
-      $("#movie-money").text("Budget: " + data.movies.slice(-1)[0].budget + " Revenue: " + data.movies.slice(-1)[0].revenue)
-      $("#movie-release-date").text("Release date: " + data.movies.slice(-1)[0].releaseDate)
+      // $("#p1").css("Font-Weight","Bold").text("Here's the movie:")
+      // $("#movie-title").text("Title " + data.movies.slice(-1)[0].title)
+      // $("#movie-genres").text("Title " + data.movies.slice(-1)[0].genres)
+      // $("#movie-keywords").text("Title " + data.movies.slice(-1)[0].keywords)
+      // $("#movie-original-language").text("Original language " + data.movies.slice(-1)[0].originalLanguage)
+      // $("#movie-score").text("Score " + data.movies.slice(-1)[0].score)
+      // $("#movie-cast-score").text("Cast score: " + data.movies.slice(-1)[0].castScore)
+      // $("#movie-money").text("Finance fraction: " + data.movies.slice(-1)[0].financeFraction)
       
       //$("#img").attr("src", data.config.images.secure_base_url + 'w500/' + data.credits.cast[0].profile_path)
       
     })
   })
-  
 })
--- server.js
+++ server.js
@@ -3,28 +3,27 @@ const express = require('express');
 const uuid = require('uuid/v4')
 var session = require('express-session');
 const fetch = require('node-fetch');
+var d3 = require("d3");
 
 // Global vars
 const apiKey = process.env.MOVIEDB_API_KEY;   // the API key to TMDB API
-var d = {}                                    // global object with session and movies info
-d.movies = []
+var moviesData = {}                           // global object with session and movies info
+moviesData.ids = []
+moviesData.scores = []
+moviesData.recencies = []
+moviesData.castScores = []
+moviesData.genres = {}
+moviesData.keywords = {}
+moviesData.financialSuccesses = []
+moviesData.originalLanguages = {}
 
 // Initialise express app
 const app = express();
 app.use(express.static('public'))
 
-//TODO Download daily dump of keywords and genre IDs
-// Other Interesting data:, belongs_to_collection, genres, original_language, popularity
-// production_companies, production_countries, runtime, tagline, recommendations,
-// credits (cast & crew), images
-//Note there is crew as well against the person credits
-// TODO need a error handling when movie not found (no results)
-//TODO tell user if movie selected (first) is good (button?)
-
 // Session configuration
 app.use(session({
   genid: (req) => {
-    console.log('Inside the session middleware')
     return uuid()
   },
   secret: process.env.SESSION_SECRET,
@@ -33,15 +32,7 @@ app.use(session({
 }))
 
 app.get("/", (appRequest, appResponse) => {
-  console.log('Inside the homepage callback function')
-  console.log(appRequest.sessionID)
   appResponse.sendFile(__dirname + '/views/index.html')
-})
-
-app.get("/movie", (appRequest, appResponse) => {
-  console.log('Inside the movie function')
-  console.log(appRequest.sessionID)
-  appResponse.sendStatus(200);
 })
 
 const AsyncFetch = async url => {
@@ -54,30 +45,58 @@ const multiFetchAsync = async (appRequest, configBody, searchURL) => {
     
     var response = await (await fetch(searchURL)).json();
     const id = response.results[0].id
+    moviesData.ids.push(id)
     
-    // Movie details endpoint
+    // Movie details endpoint - ping and fetch data
     const movieURL = 'https://api.themoviedb.org/3/movie/' + id + '?api_key=' + apiKey + '&language=en-US' + 
                      '&append_to_response=keywords,recommendations,credits,images'
     var response = await (await fetch(movieURL)).json();
+    
+    // Title and overview data
     const overview = response.overview
     const title = response.title
     const originalTitle = response.original_title
-    const originalLanguage = response.original_language  // TODO "it", in config there is endpoint for getting langs
-    const revenue = response.revenue
+    
+    // Update the language data counts
+    moviesData.originalLanguages[response.original_language] = 
+      moviesData.originalLanguages[response.original_language] ?
+      moviesData.originalLanguages[response.original_language] += 1 : 1
+    
+    // Financial success - fraction revenue to budget, rounded
+    moviesData.financialSuccesses.push(Number((response.revenue/response.budget).toFixed(2)))
+    
+    // Score users gave
     const score = response.vote_average
-    const numVotes = response.vote_count    // TODO compute age in years
-    const releaseDate = response.release_date
-    const budget = response.budget    // TODO need to check for when unknown comes as 0? (same for revenue)
+    const numVotes = response.vote_count
+    moviesData.scores.push(score)
+        
+    // Update the counts for genres and keywords
     const genres = response.genres
-    const keywords = response.keywords
+    for (var i=0; i < genres.length; i++) {
+      moviesData.genres[genres[i].name] = 
+        moviesData.genres[genres[i].name] ? moviesData.genres[genres[i].name] += 1 : 1
+    }
+    const keywords = response.keywords.keywords
+    for (var i=0; i < keywords.length; i++) {
+      moviesData.keywords[keywords[i].name] = 
+        moviesData.keywords[keywords[i].name] ? moviesData.keywords[keywords[i].name] += 1 : 1
+    }
+    
+    // Compute how old is the movie in years
+    const yearsBack = new Date().getFullYear() - new Date(response.release_date).getFullYear()
+    moviesData.recencies.push(yearsBack)
+    
+    // Recommended movies
     const recommendations = response.recommendations
+    
+    // Images
     const images = response.images
     
     // Fetch data against the main cast (first 5 in order of insertion)
     var mainCast = response.credits.cast.filter((item => item.order <= 5))
     for (var i=0; i < mainCast.length; i++) {
       
-      // Person endpoint
+      // Person endpoint - ping
       var personURL = 'https://api.themoviedb.org/3/person/' + mainCast[i].id + '?api_key=' + apiKey + 
                       '&language=en-US&append_to_response=movie_credits'
       var response = await (await fetch(personURL)).json();
@@ -88,45 +107,22 @@ const multiFetchAsync = async (appRequest, configBody, searchURL) => {
       mainCast[i]['score'] = personScores.reduce((a, b) => a + b) / personScores.length
     }
         
-    // Compute the castScore (avg of cast scores) - this can be done in a less silly way
+    // Compute the castScore (avg of cast scores) and update - rounded
     var sums = []
     for(var i=0; i < mainCast.length; i++) { sums.push(mainCast[i]['score']) }
     const castScore = sums.reduce((a, b) => a + b) / mainCast.length
-    //console.log(castScore)
-            
-    //console.log(id)
-    //console.log(keywords)
-    //console.log(genres)
+    moviesData.castScores.push(Number(castScore.toFixed(2)))
     
-    d.sessionId = appRequest.sessionID
-    d.movies.push({'id': id,
-            'title': title,
-            'originalTitle': originalTitle,
-            'originalLanguage': originalLanguage,
-            'overview': overview,
-            'mainCast': mainCast,
-            'config': configBody,
-            'score': score,
-            'numVotes': numVotes,
-            'castScore': castScore,
-            'revenue': revenue,
-            'budget': budget,
-            'releaseDate': releaseDate
-           })
-    
-    // Get movies by discovery ??
-    
-    return d;
+    moviesData.sessionId = appRequest.sessionID
+        
+    return moviesData;
   } catch (error) {
     console.log(error);
   }
 };
 
 app.post("/movie", (appRequest, appResponse) => {
-  
-  console.log('Inside the movie function')
-  console.log(appRequest.sessionID)
-  
+    
   // Config endpoint to get base URL for images
   const configURL = 'https://api.themoviedb.org/3/configuration?api_key=' + apiKey
   // Search endpoint to get movie based on query
